1dda129e66a0999a6564ecc6cda99ceafaac0619
haole1996 <haole042@gmail.com>
2023-02-12 21:56:43 +0700

    hahha

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+});
+
+$('.switch-view').on('click', function(){
+  $('.content').toggleClass('container-small container-fluid');
+});
